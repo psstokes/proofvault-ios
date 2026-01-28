18bdc5bcf3dbe96ef5551ab8aa4ec3ed12b2afde
Paul Spencer Stokes <psstokes@me.com>
2026-01-28 17:25:18 +0000

    M0: Initial ProofVault SwiftUI project

---
+++ PrroofVault/PrroofVault/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PrroofVault
+//
+//  Created by Home on 28/01/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PrroofVault/PrroofVault/PrroofVaultApp.swift
@@ -0,0 +1,17 @@
+//
+//  PrroofVaultApp.swift
+//  PrroofVault
+//
+//  Created by Home on 28/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct PrroofVaultApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
